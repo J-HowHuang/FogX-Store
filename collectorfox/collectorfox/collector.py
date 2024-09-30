f69521a895994bbf20cdca299fcecd1353a4fed4
asd08573064 <xdmanwww@gmail.com>
2024-09-30 01:11:15 -0400

    feat: rlds to lance api

--- collectorfox/collectorfox/collector.py
+++ collectorfox/collectorfox/collector.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import tensorflow as tf
-import lancedb
+import lance
+import pyarrow as pa
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -22,21 +24,20 @@ def index():
     return 'Data written to LanceDB successfully!'
 
 @app.route('/')
-def rlds_to_lance(ds: tf.data.Dataset):
-    
-    # Connect to LanceDB
-    db = lancedb.connect('your_database_url')
-    
-    
-    # Create a new collection in LanceDB
-    collection = db.create_collection('your_collection_name')
-    # Read RLDS dataset
+def rlds_to_lance(ds: tf.data.Dataset, uri: str):  
+    episode_table = defaultdict(list)
     for episode in ds:
         episode.pop('steps') # Remove steps from the episode (Rest of the data is metadata)
-        for _, metadata in episode.items():
-            collection.insert(metadata)
-    
-    return 'Data written to LanceDB successfully!'
+        for key, value in episode['episode_metadata'].items():
+            for key, value in episode['episode_metadata'].items():
+                value = value.numpy()
+                if isinstance(value, bytes):
+                    value = str(value)
+                episode_table[key].append(value)
+        arrow_table = pa.table(episode_table)
+
+    lance.write_dataset(arrow_table, uri, max_rows_per_group=8192, max_rows_per_file=1024*1024)
+
 
 if __name__ == '__main__':
     app.run()
